895e3aceb9d6a1d4a4bf182554f0989ada40f2fa
recsi <re.csi2015@gmail.com>
2017-07-09 15:36:39 -0300

    concluindo

--- build/generated-sources/ap-source-output/pacote/Livros_.java
+++ build/generated-sources/ap-source-output/pacote/Livros_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import pacote.Autores;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-09T13:52:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-09T15:27:41")
 @StaticMetamodel(Livros.class)
 public class Livros_ { 
 
