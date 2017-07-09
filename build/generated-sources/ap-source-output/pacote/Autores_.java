5a5b39561fad62dbdddca62d100f250bef954a55
recsi <re.csi2015@gmail.com>
2017-07-09 13:54:57 -0300

    editando

--- build/generated-sources/ap-source-output/pacote/Autores_.java
+++ build/generated-sources/ap-source-output/pacote/Autores_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import pacote.Livros;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-09T13:33:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-09T13:52:03")
 @StaticMetamodel(Autores.class)
 public class Autores_ { 
 
--- build/generated-sources/ap-source-output/pacote/Usuarios_.java
+++ build/generated-sources/ap-source-output/pacote/Usuarios_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-09T13:33:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-09T13:52:03")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
